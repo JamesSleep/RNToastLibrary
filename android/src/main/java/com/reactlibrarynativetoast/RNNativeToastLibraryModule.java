9e8e20fb4c2fd5c1e793585d3c9d2848eed69504
김동하 <supercurve2256@gmail.com>
2022-08-16 13:42:45 +0900

    android, ios module set

--- android/src/main/java/com/reactlibrarynativetoast/RNNativeToastLibraryModule.java
+++ android/src/main/java/com/reactlibrarynativetoast/RNNativeToastLibraryModule.java
@@ -1,5 +1,8 @@
 
-package com.reactlibrary;
+package com.reactlibrarynativetoast;
+
+import android.content.Context;
+import android.widget.Toast;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -18,5 +21,11 @@ public class RNNativeToastLibraryModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNNativeToastLibrary";
+  }
+
+  @ReactMethod
+  public void show(String text) {
+    Context context = getReactApplicationContext();
+    Toast.makeText(context, text, Toast.LENGTH_LONG).show();
   }
 }
